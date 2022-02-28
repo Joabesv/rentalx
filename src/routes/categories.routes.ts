0a05efeed7f89068f0b2cb32bd19c90cc6c78a8a
Joabesv <joabevarjao123@gmail.com>
2022-02-28 20:37:17 -0300

    criando repositorio de categorias para aliviar a responsabilidade do routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express';
-import { v4 as uuidV4 } from 'uuid';
+
+import { CategoriesRepository } from '../repositories/CategoriesRepositories';
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body;
-  const category = { name, description, id: uuidV4() };
 
-  categories.push(category);
+  categoriesRepository.create({ name, description });
 
   return res.status(201).send();
 });
