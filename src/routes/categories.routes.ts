99be10ac2aa5771b14db7c7f6d0bea762692501e
Joabesv <joabevarjao123@gmail.com>
2022-02-28 20:54:20 -0300

    validando o cadastro de categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -9,6 +9,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return res.status(400).json({ error: 'Category already exists' });
+  }
+
   categoriesRepository.create({ name, description });
 
   return res.status(201).send();
