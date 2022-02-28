9182ccde706a3690692117dccc1f40f323551325
Joabesv <joabevarjao123@gmail.com>
2022-02-28 19:46:24 -0300

    criando categoria e inserindo id com a lib uuid

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { v4 as uuidV4 } from 'uuid';
+
+const categoriesRoutes = Router();
+
+const categories = [];
+
+categoriesRoutes.post('/', (req, res) => {
+  const { name, description } = req.body;
+  const category = { name, description, id: uuidV4() };
+
+  categories.push(category);
+
+  return res.status(201).send();
+});
+
+export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
-const app = express();
+import { categoriesRoutes } from './routes/categories.routes';
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Hello World!' });
-});
+const app = express();
+app.use(express.json());
+
+app.use('/categories', categoriesRoutes);
 
 app.listen(3333, () => console.log(`Server Started bro`));
